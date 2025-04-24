9ec769fb849284b98e33552ef9e63a97aff51e99
conradosousa <conradojns@hotmail.com>
2025-04-24 16:53:45 -0300

    js banda

---
+++ atividades-switch-case.js
@@ -0,0 +1,51 @@
+// let pontoCardeal = "sudeste";
+
+// switch(pontoCardeal){
+//     case "norte":
+//         console.log("Pra cima");
+//         break;
+
+
+//     case "sul":
+//         console.log("Pra baixo");
+//         break;
+
+
+//     case "leste":
+//         console.log("Pra direita");
+//         break;
+
+
+//     case "oeste":
+//         console.log("Pra esquerda");
+//         break;
+//         default:
+//             console.log("Informe uma opção válida");
+
+// }
+
+// let menuDiario = 4;
+
+// switch (menuDiario) {
+//     case 1:
+//         console.log("peixe");
+//         break;
+
+//     case 2:
+//         console.log("frango");
+//         break;
+
+//     case 3:
+//         console.log("carne");
+//         break;
+
+//     default:
+//         console.log("opção invalida!");
+//         break;
+
+
+
+
+
+// }
+
---
+++ index.js
@@ -0,0 +1,16 @@
+let mes = prompt("Digite o mês do seu nascimento(ex: jan):");
+let dia = prompt("Digite o dia do seu nascimento(ex: 1-31):");
+alert(`mês ${mes} dia ${dia}`);
+
+let nomeDabanda = "";
+
+switch (mes) {
+    case "jan":
+        nomeDabanda = "Capirotos";
+        
+        break;
+}
+alert(`O nome da sua banda é ${nomeDabanda}`)
+
+
+
